78833f637663e3d0faf0b80dd410174b1356cbac
Qz <qzcurious@gmail.com>
2021-04-16 10:59:59 +0800

    Integrate Vue Sigma
    npm i -D sass-loader@10 sass  # vue3 還不能用 sass-loader@11
    npm install @fullcalendar/core --save
    npm install @fullcalendar/daygrid --save
    npm install @fullcalendar/timegrid --save
    npm install @fullcalendar/interaction --save
    npm i prismjs
    npm i chart.js@2  # primevue 還不支援 chartjs3

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,235 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div :class="containerClass" @click="onWrapperClick">
+    <AppTopBar @menu-toggle="onMenuToggle" />
+
+    <transition name="layout-sidebar">
+      <div
+        :class="sidebarClass"
+        @click="onSidebarClick"
+        v-show="isSidebarVisible()"
+      >
+        <div class="layout-logo">
+          <router-link to="/">
+            <img alt="Logo" :src="logo" />
+          </router-link>
+        </div>
+
+        <AppProfile />
+        <AppMenu :model="menu" @menuitem-click="onMenuItemClick" />
+      </div>
+    </transition>
+
+    <div class="layout-main">
+      <router-view />
+    </div>
+
+    <AppConfig
+      :layoutMode="layoutMode"
+      :layoutColorMode="layoutColorMode"
+      @layout-change="onLayoutChange"
+      @layout-color-change="onLayoutColorChange"
+    />
+
+    <AppFooter />
   </div>
-  <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import AppTopBar from "./AppTopbar.vue";
+import AppProfile from "./AppProfile.vue";
+import AppMenu from "./AppMenu.vue";
+import AppConfig from "./AppConfig.vue";
+import AppFooter from "./AppFooter.vue";
 
-#nav {
-  padding: 30px;
-}
+export default {
+  data() {
+    return {
+      layoutMode: "static",
+      layoutColorMode: "dark",
+      staticMenuInactive: false,
+      overlayMenuActive: false,
+      mobileMenuActive: false,
+      menu: [
+        {
+          label: "Menu Hierarchy",
+          icon: "pi pi-fw pi-search",
+          items: [
+            {
+              label: "Submenu 1",
+              icon: "pi pi-fw pi-bookmark",
+              items: [
+                {
+                  label: "Submenu 1.1",
+                  icon: "pi pi-fw pi-bookmark",
+                  items: [
+                    { label: "Submenu 1.1.1", icon: "pi pi-fw pi-bookmark" },
+                    { label: "Submenu 1.1.2", icon: "pi pi-fw pi-bookmark" },
+                    { label: "Submenu 1.1.3", icon: "pi pi-fw pi-bookmark" },
+                  ],
+                },
+                {
+                  label: "Submenu 1.2",
+                  icon: "pi pi-fw pi-bookmark",
+                  items: [
+                    { label: "Submenu 1.2.1", icon: "pi pi-fw pi-bookmark" },
+                    { label: "Submenu 1.2.2", icon: "pi pi-fw pi-bookmark" },
+                  ],
+                },
+              ],
+            },
+            {
+              label: "Submenu 2",
+              icon: "pi pi-fw pi-bookmark",
+              items: [
+                {
+                  label: "Submenu 2.1",
+                  icon: "pi pi-fw pi-bookmark",
+                  items: [
+                    { label: "Submenu 2.1.1", icon: "pi pi-fw pi-bookmark" },
+                    { label: "Submenu 2.1.2", icon: "pi pi-fw pi-bookmark" },
+                    { label: "Submenu 2.1.3", icon: "pi pi-fw pi-bookmark" },
+                  ],
+                },
+                {
+                  label: "Submenu 2.2",
+                  icon: "pi pi-fw pi-bookmark",
+                  items: [
+                    { label: "Submenu 2.2.1", icon: "pi pi-fw pi-bookmark" },
+                    { label: "Submenu 2.2.2", icon: "pi pi-fw pi-bookmark" },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  },
+  watch: {
+    $route() {
+      this.menuActive = false;
+      this.$toast.removeAllGroups();
+    },
+  },
+  methods: {
+    onWrapperClick() {
+      if (!this.menuClick) {
+        this.overlayMenuActive = false;
+        this.mobileMenuActive = false;
+      }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+      this.menuClick = false;
+    },
+    onMenuToggle() {
+      this.menuClick = true;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+      if (this.isDesktop()) {
+        if (this.layoutMode === "overlay") {
+          if (this.mobileMenuActive === true) {
+            this.overlayMenuActive = true;
+          }
+
+          this.overlayMenuActive = !this.overlayMenuActive;
+          this.mobileMenuActive = false;
+        } else if (this.layoutMode === "static") {
+          this.staticMenuInactive = !this.staticMenuInactive;
+        }
+      } else {
+        this.mobileMenuActive = !this.mobileMenuActive;
+      }
+
+      event.preventDefault();
+    },
+    onSidebarClick() {
+      this.menuClick = true;
+    },
+    onMenuItemClick(event) {
+      if (event.item && !event.item.items) {
+        this.overlayMenuActive = false;
+        this.mobileMenuActive = false;
+      }
+    },
+    onLayoutChange(layoutMode) {
+      this.layoutMode = layoutMode;
+    },
+    onLayoutColorChange(layoutColorMode) {
+      this.layoutColorMode = layoutColorMode;
+    },
+    addClass(element, className) {
+      if (element.classList) element.classList.add(className);
+      else element.className += " " + className;
+    },
+    removeClass(element, className) {
+      if (element.classList) element.classList.remove(className);
+      else
+        element.className = element.className.replace(
+          new RegExp(
+            "(^|\\b)" + className.split(" ").join("|") + "(\\b|$)",
+            "gi"
+          ),
+          " "
+        );
+    },
+    isDesktop() {
+      return window.innerWidth > 1024;
+    },
+    isSidebarVisible() {
+      if (this.isDesktop()) {
+        if (this.layoutMode === "static") return !this.staticMenuInactive;
+        else if (this.layoutMode === "overlay") return this.overlayMenuActive;
+        else return true;
+      } else {
+        return true;
+      }
+    },
+  },
+  computed: {
+    containerClass() {
+      return [
+        "layout-wrapper",
+        {
+          "layout-overlay": this.layoutMode === "overlay",
+          "layout-static": this.layoutMode === "static",
+          "layout-static-sidebar-inactive":
+            this.staticMenuInactive && this.layoutMode === "static",
+          "layout-overlay-sidebar-active":
+            this.overlayMenuActive && this.layoutMode === "overlay",
+          "layout-mobile-sidebar-active": this.mobileMenuActive,
+          "p-input-filled": this.$appState.inputStyle === "filled",
+          "p-ripple-disabled": this.$primevue.config.ripple === false,
+        },
+      ];
+    },
+    sidebarClass() {
+      return [
+        "layout-sidebar",
+        {
+          "layout-sidebar-dark": this.layoutColorMode === "dark",
+          "layout-sidebar-light": this.layoutColorMode === "light",
+        },
+      ];
+    },
+    logo() {
+      return this.layoutColorMode === "dark"
+        ? "assets/layout/images/logo-white.svg"
+        : "assets/layout/images/logo.svg";
+    },
+  },
+  beforeUpdate() {
+    if (this.mobileMenuActive)
+      this.addClass(document.body, "body-overflow-hidden");
+    else this.removeClass(document.body, "body-overflow-hidden");
+  },
+  components: {
+    AppTopBar: AppTopBar,
+    AppProfile: AppProfile,
+    AppMenu: AppMenu,
+    AppConfig: AppConfig,
+    AppFooter: AppFooter,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./App.scss";
 </style>
