5350591e6f1dbab2364ef484bcd0962c64bb329a
Qz <qzcurious@gmail.com>
2021-05-07 15:38:53 +0800

    修改 ToastService
    新增 alias function
    預設 life 3000

--- src/service/ToastService.js
+++ src/service/ToastService.js
@@ -1,8 +1,18 @@
 const ToastService = {
   init: toastService => {
-    ToastService.add = toastService.add;
+    ToastService.add = ({ life, ...args }) =>
+      toastService.add({ life: 3000, ...args });
     ToastService.removeGroup = toastService.removeGroup;
     ToastService.removeAllGroup = toastService.removeAllGroup;
+
+    ToastService.success = args =>
+      toastService.add({ ...args, severity: "success" });
+    ToastService.info = args =>
+      toastService.add({ ...args, severity: "info" });
+    ToastService.warn = args =>
+      toastService.add({ ...args, severity: "warn" });
+    ToastService.error = args =>
+      toastService.add({ ...args, severity: "error" });
   }
 };
 
