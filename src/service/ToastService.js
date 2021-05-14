062e1b2fab59b738d727682af55154343dea5069
Qz <qzcurious@gmail.com>
2021-05-14 14:58:26 +0800

    修正 ToastService 沒有預設 3秒消失

--- src/service/ToastService.js
+++ src/service/ToastService.js
@@ -1,18 +1,18 @@
 const ToastService = {
   init: toastService => {
-    ToastService.add = ({ life, ...args }) =>
+    ToastService.add = ({ ...args }) =>
       toastService.add({ life: 3000, ...args });
     ToastService.removeGroup = toastService.removeGroup;
     ToastService.removeAllGroup = toastService.removeAllGroup;
 
     ToastService.success = args =>
-      toastService.add({ ...args, severity: "success" });
+      ToastService.add({ ...args, severity: "success" });
     ToastService.info = args =>
-      toastService.add({ ...args, severity: "info" });
+      ToastService.add({ ...args, severity: "info" });
     ToastService.warn = args =>
-      toastService.add({ ...args, severity: "warn" });
+      ToastService.add({ ...args, severity: "warn" });
     ToastService.error = args =>
-      toastService.add({ ...args, severity: "error" });
+      ToastService.add({ ...args, severity: "error" });
   }
 };
 
