a02d743166f164da3e808700b085bbb77be6fc94
Qz <qzcurious@gmail.com>
2021-05-20 16:18:37 +0800

    component StatusDropdown

---
+++ src/components/StatusDropdown.vue
@@ -0,0 +1,33 @@
+<template>
+  <Dropdown
+    :label="label"
+    :options="[
+      { label: $t('disabled'), value: 0 },
+      { label: $t('enabled'), value: 1 },
+    ]"
+    :modelValue="modelValue"
+    @update:modelValue="(value) => $emit('update:modelValue', value)"
+  />
+</template>
+
+<script>
+import Dropdown from "./Dropdown.vue";
+import i18n from "../i18n";
+
+export default {
+  components: { Dropdown },
+  props: {
+    modelValue: Number,
+    label: { type: String, default: () => i18n.global.t("status") },
+  },
+  data() {
+    return {
+      value: null,
+    };
+  },
+  emits: ["update:modelValue"],
+};
+</script>
+
+<style>
+</style>
