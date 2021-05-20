e7b7496fad6a287c707ce46f6c3effe1364df8d0
Qz <qzcurious@gmail.com>
2021-05-20 17:35:15 +0800

    component Search

---
+++ src/components/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <Button
+    type="submit"
+    icon="pi pi-search"
+    :label="$t('form.search')"
+    :class="{ 'p-button-outlined': true, ...$attrs.class }"
+  />
+</template>
+
+<script>
+import Button from "primevue/button";
+import i18n from "../i18n";
+
+export default {
+  inheritAttrs: false,
+  components: { Button },
+  emits: ["update:modelValue"],
+  props: {
+    modelValue: Number,
+    label: { type: String, default: () => i18n.global.t("form.search") },
+  },
+};
+</script>
